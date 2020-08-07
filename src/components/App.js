38edcb79ef46fc1d5f8ab4bd4db3e1fdef0a24de
Lenny Peters <mr.lennypeters@gmail.com>
2020-08-06 21:10:20 -0700

    refactor: 💡 hooks
    created a hooks directory for abstraction

--- src/components/App.js
+++ src/components/App.js
@@ -2,42 +2,26 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "../components/SearchBar";
 import VideoDetail from "../components/VideoDetail";
 import VideoList from "../components/VideoList";
-import youtube from "../api/youtube";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("bacon cheeseburgers");
 
   useEffect(() => {
-    onTermSubmit("Single origin coffee");
-  }, [setSelectedVideo]);
-
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
-
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-    const { data } = response;
-    console.log(data);
-    setVideos(data.items);
-    setSelectedVideo(data.items[0]);
-  };
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
